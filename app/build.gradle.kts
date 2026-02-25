45f47465bd3f114911dfc653d219023a337cc36d
salcxjo <salar.miri@gmail.com>
2026-02-24 18:25:15 -0700

    demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.androiduitesting"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.androiduitesting"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34  // match compileSdk
         versionCode = 1
         versionName = "1.0"
 
@@ -32,11 +32,16 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
